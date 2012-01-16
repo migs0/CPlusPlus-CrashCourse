5d103152e04de6ec13430b98ec3eff099ffbb321
Miguel Zuniga <miguel_zuniga21@yahoo.com>
2012-01-16 22:47:08 +0800

    added welcome code

---
+++ 00.Welcome.cpp
@@ -0,0 +1,26 @@
+/**
+ * C++
+ * Welcome
+ *
+ * This is a minimal start-up C++ Program
+ */
+#include <iostream>
+main(){
+	// This will diplay the text inside the double quotes
+	std::cout << "Programming\nin \"C++\"";
+	return 1;
+}
+
+/**
+
+Escape Characters
+
+\n     Newline
+\t     Horizontal tab
+\r     Carriage return
+\a     Alert
+\\     Backslash
+\'     Single Quote
+\"     Double Quote
+
+**/
