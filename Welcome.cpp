2fac111ce9f9400ad863190b2d49fe6843403144
Miguel Zuniga <miguel_zuniga21@yahoo.com>
2012-01-17 20:04:58 +0800

    Sir, just practicing basic escape characters

---
+++ Welcome.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+	
+	std::cout<<"Hello World!\n";
+	std::cout<<"My name is Miguel\n";
+	std::cout<<"I want to be a successful Computer Engineer \t\t..someday\n\n";
+	std::cout<<"Just practicing basic escape characters\n";
+	std::cout<<"This sentence will be  gone. \rThe words before this sentence is not shown.\n";
+	std::cout<<"You heard a beeping sound.\a\n\n";
+	std::cout<<"  O/ \n";
+	std::cout<<" /|    HI!\n";
+	std::cout<<"  |   \n";
+	std::cout<<" / \\ \n\n";
+	std::cout<<'a';std::cout<<'b';std::cout<<'c';std::cout<<'d';std::cout<<"\n\n";
+	return 1; 
+}
+
